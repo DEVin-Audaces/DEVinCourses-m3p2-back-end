4a3a284f9a83e96cd907bf165b48143f65867ae0
eduardo-borges-goncalves <alemaobrk1@gmail.com>
2022-11-10 11:32:08 -0300

    fix: improve dataAnnotations key by entity rules

--- DEVCoursesAPI/Data/Models/TopicUser.cs
+++ DEVCoursesAPI/Data/Models/TopicUser.cs
@@ -1,19 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DEVCoursesAPI.Data.Models
 {
     public class TopicUser
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+        public bool Completed { get; set; }
+
         [ForeignKey("Training")]
         public Guid TrainingId { get; set; }
+        public Training? Training { get; set; }
+
         [ForeignKey("Users")]
         public Guid UserId { get; set; }
+        public Users? Users { get; set; }
+
         [ForeignKey("Topic")]
         public Guid TopicId { get; set; }
-        public bool Completed { get; set; }
-        public Training? Training { get; set; }
-        public Users? Users { get; set; }
         public Topic? Topic { get; set; }
     }
 }
--- DEVCoursesAPI/Data/Models/TrainingUser.cs
+++ DEVCoursesAPI/Data/Models/TrainingUser.cs
@@ -1,17 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DEVCoursesAPI.Data.Models
 {
     public class TrainingUser
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
-        [ForeignKey("Training")]
-        public Guid TrainingId { get; set; }
-        [ForeignKey("Users")]
-        public Guid UserId { get; set; }
         public bool Completed { get; set; }
         public DateTime RegistrationDate { get; set; }
+
+        [ForeignKey("Training")]
+        public Guid TrainingId { get; set; }
         public Training? Training { get; set; }
+
+        [ForeignKey("Users")]
+        public Guid UserId { get; set; }
         public Users? Users { get; set; }
     }
 }
