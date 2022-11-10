5da579ce4636950e15b2ea3b0ee9364b7a7a1ef7
eduardo-borges-goncalves <alemaobrk1@gmail.com>
2022-11-09 21:47:41 -0300

    Feature: Create Model Conteudo_usuarios - part 2

--- DEVCoursesAPI/Data/Models/Conteudo_usuarios.cs
+++ DEVCoursesAPI/Data/Models/Conteudo_usuarios.cs
@@ -2,5 +2,10 @@
 {
     public class Conteudo_usuarios
     {
+        public Guid Id { get; set; }
+        public Guid Id_treinamento { get; set; }
+        public Guid Id_usuario { get; set; }
+        public Guid Id_conteudo { get; set; }
+        public bool Concluido { get; set; }
     }
 }
