ba29543bece86d116267562875f2c1abc0731dc9
eduardo-borges-goncalves <alemaobrk1@gmail.com>
2022-11-09 21:35:03 -0300

    feat: create model treinamento_usuarios

---
+++ DEVCoursesAPI/Data/Models/Treinamento_usuarios.cs
@@ -0,0 +1,11 @@
+﻿namespace DEVCoursesAPI.Data.Models
+{
+    public class Treinamento_usuarios
+    {
+        public Guid Id { get; set; }
+        public Guid Id_treinamento { get; set; }
+        public Guid Id_usuario { get; set; }
+        public bool Concluido { get; set; }
+        public DateTime Data_matricula { get; set; }
+    }
+}
