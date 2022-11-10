b2ffa5ce93f8c4344d6887d63310015e6e831029
eduardo-borges-goncalves <alemaobrk1@gmail.com>
2022-11-09 21:23:51 -0300

    feat: create controller treinamentos

--- DEVCoursesAPI/Data/Models/Treinamento_usuarios.cs
+++ DEVCoursesAPI/Data/Models/Treinamento_usuarios.cs
@@ -2,5 +2,10 @@
 {
     public class Treinamento_usuarios
     {
+        public Guid Id { get; set; }
+        public Guid Id_treinamento { get; set; }
+        public Guid Id_usuario { get; set; }
+        public bool Concluido { get; set; }
+        public DateTime Data_matricula { get; set; }
     }
 }
