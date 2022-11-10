1644feef15dc3b5f3e2de661912cf9fd6750c18a
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-10 10:24:31 -0300

    feat: create model Module

---
+++ DEVCoursesAPI/Data/Models/Module.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DEVCoursesAPI.Data.Models
+{
+    public class Module
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Index { get; set; }
+        [ForeignKey("Training")]
+        public Guid TrainingId { get; set; }
+        public Training? Training { get; set; }
+
+    }
+}
