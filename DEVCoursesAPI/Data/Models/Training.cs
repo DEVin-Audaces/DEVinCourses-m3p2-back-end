4fde421cb9fbd99d5508977499d041a754395c27
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-10 10:24:16 -0300

    feat: create model Training

---
+++ DEVCoursesAPI/Data/Models/Training.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DEVCoursesAPI.Data.Models
+{
+    public class Training
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public double Duration { get; set; }
+        public string Instructor { get; set; }
+        public Guid Author { get; set; }
+        public bool Active { get; set; }
+    }
+}
