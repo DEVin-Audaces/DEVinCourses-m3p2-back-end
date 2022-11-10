b3af725f2963da032aa788fd6fff5bc087ea3117
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-10 10:24:40 -0300

    feat: create model Topic

---
+++ DEVCoursesAPI/Data/Models/Topic.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DEVCoursesAPI.Data.Models
+{
+    public class Topic
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public int Index { get; set; }
+        [ForeignKey("Module")]
+        public Guid ModuleId { get; set; }
+        public Module? Module { get; set; }
+    }
+}
