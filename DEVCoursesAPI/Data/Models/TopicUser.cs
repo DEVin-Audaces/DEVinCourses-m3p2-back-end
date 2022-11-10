0f36c01e8699aa50b1e5673b71693daa854a56ad
eduardo-borges-goncalves <alemaobrk1@gmail.com>
2022-11-10 10:50:07 -0300

    feat: insert data nottations at models and models at context

---
+++ DEVCoursesAPI/Data/Models/TopicUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DEVCoursesAPI.Data.Models
+{
+    public class TopicUser
+    {
+        public Guid Id { get; set; }
+        [ForeignKey("Training")]
+        public Guid TrainingId { get; set; }
+        [ForeignKey("Users")]
+        public Guid UserId { get; set; }
+        [ForeignKey("Topic")]
+        public Guid TopicId { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ DEVCoursesAPI/Data/Models/TrainingUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DEVCoursesAPI.Data.Models
+{
+    public class TrainingUser
+    {
+        public Guid Id { get; set; }
+        [ForeignKey("Training")]
+        public Guid TrainingId { get; set; }
+        [ForeignKey("Users")]
+        public Guid UserId { get; set; }
+        public bool Completed { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
