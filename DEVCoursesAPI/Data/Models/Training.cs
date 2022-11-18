8eeadd9535b2aa055422f155583503b1d71354f6
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-18 15:22:39 -0300

    feat: add field Modules and convert operator to ReadDto

--- DEVCoursesAPI/Data/Models/Training.cs
+++ DEVCoursesAPI/Data/Models/Training.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using DEVCoursesAPI.Data.DTOs.TrainingDTO;
 
 namespace DEVCoursesAPI.Data.Models
 {
@@ -14,5 +15,21 @@ namespace DEVCoursesAPI.Data.Models
         public string Instructor { get; set; }
         public Guid Author { get; set; }
         public bool Active { get; set; }
+        public List<Module> Modules { get; set; }
+
+        public static explicit operator ReadTrainingDto(Training training)
+        {
+            return new ReadTrainingDto
+            {
+                Id = training.Id,
+                Name = training.Name,
+                Summary = training.Summary,
+                Duration = training.Duration,
+                Instructor = training.Instructor,
+                Author = training.Author,
+                Active = training.Active,
+                Modules = training.Modules.OrderBy(m => m.Index).Select(m => (ReadModuleDto)m),
+            };
+        }
     }
 }
