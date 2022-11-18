18cfe81e8e652e0a2b2366942e6bb7c7e0318adc
gmvilela <gm.vilela@uol.com.br>
2022-11-17 22:19:02 -0300

    feat: Create Endpoint GetTraining

--- DEVCoursesAPI/Controllers/TrainingsController.cs
+++ DEVCoursesAPI/Controllers/TrainingsController.cs
@@ -1,5 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DEVCoursesAPI.Data.Context;
+using DEVCoursesAPI.Data.DTOs;
+using DEVCoursesAPI.Data.Models;
+using DEVCoursesAPI.Services;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using DEVCoursesAPI.Repositories;
 
 namespace DEVCoursesAPI.Controllers
 {
@@ -7,14 +22,52 @@ namespace DEVCoursesAPI.Controllers
     [ApiController]
     public class TrainingsController : ControllerBase
     {
-        private readonly ILogger<UsersController> _logger;
+        private readonly ILogger<TrainingsController> _logger;
         private readonly IOptions<TrainingsController> _tokenSettings;
+        private readonly ITrainingService _trainingService;
 
-        public TrainingsController(IOptions<TrainingsController> tokenSettings, ILogger<UsersController> logger)
+
+        public TrainingsController(IOptions<TrainingsController> tokenSettings, 
+            ILogger<TrainingsController> logger, ITrainingService trainingService)
         {
             _tokenSettings = tokenSettings;
             _logger = logger;
+            _trainingService = trainingService;
         }
 
+        /// <summary>
+        /// Visualizar a tela de treinamentos
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <returns>Retorna os treinamentos de acordo com o Id do usuário inserido</returns>
+        /// <response code = "200">Retorna Lista de Treinamentos</response>
+        /// <response code = "500">Erro execução</response>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<ActionResult> Get([FromQuery] [Required] Guid UserId)
+        {
+
+            try
+            {
+                _logger.LogInformation($"Class:{nameof(TrainingsController)}-Method:{nameof(Get)}");
+                var trainings = _trainingService.UserLoginTrainingsList(UserId);
+                
+                if (trainings == null)
+                    return NotFound();
+
+                return Ok(trainings);
+
+            }
+
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Controller:{nameof(TrainingsController)}-Method:{nameof(Get)}");
+                return StatusCode(500);
+            }
+
+
+        }
     }
 }
