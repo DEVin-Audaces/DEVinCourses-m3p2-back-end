2083512c7da986e5b0d6c7caae3a7d36db5bb94c
gmvilela <gm.vilela@uol.com.br>
2022-11-17 22:36:33 -0300

    feat: Create Method in TrainingService and Scoped

---
+++ DEVCoursesAPI/Services/ITrainingService.cs
@@ -0,0 +1,10 @@
+﻿using DEVCoursesAPI.Data.DTOs;
+using DEVCoursesAPI.Data.Models;
+
+namespace DEVCoursesAPI.Services
+{
+    public interface ITrainingService
+    {
+        public List<TrainingNotRegistered> UserLoginTrainingsList(Guid userId);
+    }
+}
---
+++ DEVCoursesAPI/Services/TrainingService.cs
@@ -0,0 +1,26 @@
+﻿using DEVCoursesAPI.Data.DTOs;
+using DEVCoursesAPI.Data.Models;
+using DEVCoursesAPI.Repositories;
+
+namespace DEVCoursesAPI.Services
+{
+    
+        public class TrainingService : ITrainingService
+        {
+            private readonly ITrainingRepository<Training> _trainingRepository;
+
+            public TrainingService(ITrainingRepository<Training> trainingRepository)
+            {
+                _trainingRepository = trainingRepository;
+            }
+
+
+            public List<TrainingNotRegistered> UserLoginTrainingsList(Guid userId)
+            {
+                return _trainingRepository.UserLoginTrainingsList(userId);
+            }
+        
+        }
+
+    
+}
