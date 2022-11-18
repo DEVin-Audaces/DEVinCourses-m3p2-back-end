b8f4b3e47eb7fbc79d841a46cf128b23d8b2883f
gmvilela <gm.vilela@uol.com.br>
2022-11-17 22:25:28 -0300

    feat: Create UserLoginTrainingsList in ITrainingRepository

---
+++ DEVCoursesAPI/Repositories/ITrainingRepository.cs
@@ -0,0 +1,11 @@
+﻿using DEVCoursesAPI.Data.DTOs;
+using DEVCoursesAPI.Data.Models;
+
+namespace DEVCoursesAPI.Repositories
+{
+        public interface ITrainingRepository<TEntity>: IEntity<TEntity>
+        {
+        public List<TrainingNotRegistered> UserLoginTrainingsList(Guid userId);
+
+        }
+}
---
+++ DEVCoursesAPI/Repositories/TrainingRepository.cs
@@ -0,0 +1,129 @@
+﻿using DEVCoursesAPI.Data.Context;
+using DEVCoursesAPI.Data.DTOs;
+using DEVCoursesAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace DEVCoursesAPI.Repositories
+{
+    public class TrainingRepository : ITrainingRepository<Training>
+    {
+        private readonly DEVCoursesContext _context;
+
+        public TrainingRepository(DEVCoursesContext context)
+        {
+            _context = context;
+        }
+
+        public Guid Add(Training model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Training model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TrainingNotRegistered> UserLoginTrainingsList(Guid userId)
+        {
+
+
+            var trainings = _context.Trainings.ToList();
+            var trainingsUsers = _context.TrainingUsers.Where(x => x.UserId == userId).ToList();
+
+            List<TrainingNotRegistered> FilteredList = new List<TrainingNotRegistered>();
+
+            foreach (var training in trainings)
+            {
+                foreach (var trainingUser in trainingsUsers)
+                {
+                    if (training.Id == trainingUser.TrainingId)
+                    {
+                        var newTraining = new TrainingNotRegistered()
+                        {
+                            Id = training.Id,
+                            Name = training.Name,
+                            Summary = training.Summary,
+                            Duration = training.Duration,
+                            Instructor = training.Instructor,
+                            Author = training.Author,
+                            Active = training.Active
+                        };
+
+                        FilteredList.Add(newTraining);
+                    } 
+                }
+            }
+
+            trainings.RemoveAll(x => trainingsUsers.Any(y => x.Id == y.TrainingId));
+            trainings.ForEach(training => 
+            {
+                var newTraining = new TrainingNotRegistered()
+                {
+                    Id = training.Id,
+                    Name = training.Name,
+                    Summary = training.Summary,
+                    Duration = training.Duration,
+                };
+                FilteredList.Add(newTraining);
+            });
+
+            FilteredList.ForEach(x => Console.WriteLine(x.Instructor + "    " + x.Id));
+            return FilteredList;
+
+            
+
+
+
+
+
+            //var trainings = _context.Trainings.ToList();
+            //var trainingsUsers = _context.TrainingUsers.Where(x => x.UserId == userId).ToList();
+            //var listaTreinamentosNaoMatriculado = new List<Training>();
+
+            //List<TrainingNotRegistered> FilteredList = new List<TrainingNotRegistered>();
+
+            //foreach (var training in trainings)
+            //{
+            //    foreach (var trainingUser in trainingsUsers)
+            //    {
+            //        if (training.Id == trainingUser.TrainingId)
+            //        {
+            //            var newTraining = new TrainingNotRegistered()
+            //            {
+            //                Id = training.Id,
+            //                Name = training.Name,
+            //                Summary = training.Summary,
+            //                Duration = training.Duration,
+            //                Instructor = training.Instructor,
+            //                Author = training.Author,
+            //                Active = training.Active
+            //            };
+            //            FilteredList.Add(newTraining);
+
+            //        }else
+            //        {
+
+            //        }
+
+            //    }
+            //}
+
+            //trainings.ForEach(training => 
+            //{
+            //    var newTraining = new TrainingNotRegistered()
+            //    {
+            //        Id = training.Id,
+            //        Name = training.Name,
+            //        Summary = training.Summary,
+            //        Duration = training.Duration
+
+            //    };
+            //    FilteredList.Add(newTraining);
+            //});
+
+            //return FilteredList;
+        }
+    }
+}
