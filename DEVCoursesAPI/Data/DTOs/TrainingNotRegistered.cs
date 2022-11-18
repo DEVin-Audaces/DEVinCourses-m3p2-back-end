ca06167b16d70a78bfd9d75b5513a27a48f09f43
gmvilela <gm.vilela@uol.com.br>
2022-11-17 22:21:47 -0300

    feat: Create DTO TrainingNotRegistered

---
+++ DEVCoursesAPI/Data/DTOs/TrainingNotRegistered.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DEVCoursesAPI.Data.DTOs
+{
+    public class TrainingNotRegistered
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public double Duration { get; set; }
+        public string? Instructor { get; set; }
+        public Guid? Author { get; set; }
+        public bool? Active { get; set; }
+
+    }
+}
