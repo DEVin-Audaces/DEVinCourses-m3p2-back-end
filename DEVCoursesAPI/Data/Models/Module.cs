e65bce710bc58bdcaf15557704cc31024cebdc12
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-18 15:28:02 -0300

    feat: add Topics field and conversion to ReadDto

--- DEVCoursesAPI/Data/Models/Module.cs
+++ DEVCoursesAPI/Data/Models/Module.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using DEVCoursesAPI.Data.DTOs.ModuleDTO;
+using DEVCoursesAPI.Data.DTOs.TopicDTO;
 
 namespace DEVCoursesAPI.Data.Models
 {
@@ -10,9 +12,20 @@ namespace DEVCoursesAPI.Data.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public int Index { get; set; }
+        public List<Topic> Topics { get; set; }
         [ForeignKey("Training")]
         public Guid TrainingId { get; set; }
         public Training? Training { get; set; }
 
+        public static explicit operator ReadModuleDto(Module module)
+        {
+            return new ReadModuleDto
+            {
+                Id = module.Id,
+                Name = module.Name,
+                Index = module.Index,
+                Topics = module.Topics.OrderBy(t => t.Index).Select(t => (ReadTopicDto)t),
+            };
+        }
     }
 }
