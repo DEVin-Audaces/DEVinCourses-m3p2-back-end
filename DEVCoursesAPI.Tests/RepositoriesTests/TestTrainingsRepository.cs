0c3950d996b3edd192b46d1fd97d506aa54bd40c
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-18 21:31:18 -0300

    feat: add test for CreateTraining method

---
+++ DEVCoursesAPI.Tests/RepositoriesTests/TestTrainingsRepository.cs
@@ -0,0 +1,35 @@
+﻿using DEVCoursesAPI.Data.Models;
+using DEVCoursesAPI.Repositories;
+
+namespace DEVCoursesAPI.Tests.RepositoriesTests
+{
+    public class TestTrainingsRepository
+    {
+        private readonly Training training;
+
+        public TestTrainingsRepository()
+        {
+            training = new Training()
+            {
+                Name = "Name",
+                Summary = "Summary",
+                Duration = 10,
+                Instructor = "Instructor",
+                Author = Guid.NewGuid(),
+                Active = false
+            };
+        }
+        [Fact]
+        public async void CreateTraining_ShouldReturnGuidWhenCreatingTraining()
+        {
+            // Arrange
+            TrainingRepository repository = new(new TestCoursesDbContextFactory());
+
+            // Act
+            Guid result = await repository.CreateTraining(training);
+
+            // Assert
+            Assert.IsType<Guid>(result);
+        }
+    }
+}
