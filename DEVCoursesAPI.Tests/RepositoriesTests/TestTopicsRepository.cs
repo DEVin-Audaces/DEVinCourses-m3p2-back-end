f5c8bc1988a0f626b1b24f07345ed00a1094abcc
Pedro Capinos <pedro.capinos@gmail.com>
2022-11-19 17:02:32 -0300

    feature: create test topics repository

---
+++ DEVCoursesAPI.Tests/RepositoriesTests/TestTopicsRepository.cs
@@ -0,0 +1,23 @@
+﻿using DEVCoursesAPI.Data.Models;
+using DEVCoursesAPI.Repositories;
+
+namespace DEVCoursesAPI.Tests.RepositoriesTests
+{
+    public class TestTopicsRepository
+    {
+
+        [Fact]
+        public async void CreateAsync_ShouldReturnGuidWhenCreatingTopic()
+        {
+            // Arrange
+            ITopicsRepository repo = new TopicsRepository(new TestCoursesDbContextFactory());
+            Topic topic = new() { Content = "Content", Name = "Name", Type = "Type" };
+
+            // Act
+            Guid result = await repo.CreateAsync(topic);
+
+            // Assert
+            Assert.IsType<Guid>(result);
+        }
+    }
+}
