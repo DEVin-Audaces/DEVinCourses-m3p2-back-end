62306fe039b1d83e6b1d5df725ccf3d73beb6037
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-19 14:15:45 -0300

    feat: create RegisterdUsers model

---
+++ DEVCoursesAPI/Data/DTOs/RegisteredUsers.cs
@@ -0,0 +1,14 @@
+﻿namespace DEVCoursesAPI.Data.DTOs
+{
+    public class RegisteredUsers
+    {
+        public List<string> ActiveUsers { get; set; }
+        public List<string> FinishedUsers { get; set; }
+
+        public RegisteredUsers()
+        {
+            ActiveUsers= new List<string>();
+            FinishedUsers = new List<string>();
+        }
+    }
+}
