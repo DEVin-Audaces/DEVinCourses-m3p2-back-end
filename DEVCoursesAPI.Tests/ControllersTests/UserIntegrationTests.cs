1ac7dae583949da5d98b090d8e50b700b639c8ac
felipe <lipecoloraado@gmail.com>
2022-11-20 15:05:00 -0300

    feat/CreatTestUpdateUser

--- DEVCoursesAPI.Tests/ControllersTests/UserIntegrationTests.cs
+++ DEVCoursesAPI.Tests/ControllersTests/UserIntegrationTests.cs
@@ -62,7 +62,7 @@ public class UserIntegrationTests: ConfigurationHostApi
         string serv = "/users/CreateUser";
         Random randNum = new Random();
         long cpf = randNum.Next();
-        var body = new DataUser { Name = "José Alves", Age = 30, Email = "josealves" + cpf + "@gmail.com", CPF = cpf, Password = "AAAA2222", PasswordRepeat = "AAAA2222" };
+        var body = new DataUser { Name = "Josï¿½ Alves", Age = 30, Email = "josealves" + cpf + "@gmail.com", CPF = cpf, Password = "AAAA2222", PasswordRepeat = "AAAA2222" };
         var jsonContent = JsonConvert.SerializeObject(body);
         var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
@@ -84,7 +84,7 @@ public class UserIntegrationTests: ConfigurationHostApi
     {
         //Arrange
         string serv = "/users/CreateUser";
-        var body = new DataUser { Name = "José Alves", Age = 30, Email = "josealves@gmail.com", CPF = 15226925877, Password = "AAAAA2222", PasswordRepeat = "AAAA2222" };
+        var body = new DataUser { Name = "Josï¿½ Alves", Age = 30, Email = "josealves@gmail.com", CPF = 15226925877, Password = "AAAAA2222", PasswordRepeat = "AAAA2222" };
         var jsonContent = JsonConvert.SerializeObject(body);
         var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
@@ -94,6 +94,74 @@ public class UserIntegrationTests: ConfigurationHostApi
         //Assert
         Assert.False(returns.IsSuccessStatusCode);
 
+    }
+
+    
+    [Fact]
+    public async Task G_Consumir_Api_Update_User_Put_Sucesso()
+    {
+        //Arrange
+        string serv = "/users/LoginUser";
+        serv = "/users/LoginUser";
+        var bodyLogin = new LoginUser{Email = "lucaspereira@gmail.com", Password = "AAAA2223"};
+        var jsonContent = JsonConvert.SerializeObject(bodyLogin);
+        var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        var returns = await client.PostAsync(serv, contentString);
+
+
+        jsonContent = await returns.Content.ReadAsStringAsync();
+        var objetoResponse =  JsonConvert.DeserializeObject<JWTResult>(jsonContent);
+
+        var accessToken = objetoResponse.AccessToken;
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        
+        serv = "/users/UpdateUser";
+        var body = new DataUser{Name = "Lucas Pereira Silva", Age = 42, Email = "lucaspereira@gmail.com",CPF = 1551152260, Password = "AAAA2223", PasswordRepeat = "AAAA2223"};
+        jsonContent = JsonConvert.SerializeObject(body);
+        contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        //Act
+        returns = await client.PutAsync(serv,contentString);
+
+        //Assert
+        Assert.True(returns.IsSuccessStatusCode);
+
+        jsonContent = await returns.Content.ReadAsStringAsync();
+
+        Assert.NotEqual("", jsonContent);
+    }
+
+    [Fact]
+    public async Task H_Consumir_Api_Update_User_Put_Sem_Sucesso()
+    {
+
+        //Arrange
+        string serv = "/users/LoginUser";
+        serv = "/users/LoginUser";
+        var bodyLogin = new LoginUser{Email = "lucaspereira@gmail.com", Password = "AAAA2223"};
+        var jsonContent = JsonConvert.SerializeObject(bodyLogin);
+        var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        var returns = await client.PostAsync(serv, contentString);
+
+
+        jsonContent = await returns.Content.ReadAsStringAsync();
+        var objetoResponse =  JsonConvert.DeserializeObject<JWTResult>(jsonContent);
+
+        var accessToken = objetoResponse.AccessToken;
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        
+        serv = "/users/UpdateUser";
+        var body = new DataUser{Name = "Lucas Pereira Silva", Age = 42, Email = "lucaspereiraL@gmail.com",CPF = 1137213194, Password = "AAAA2223", PasswordRepeat = "AAAA2223"};
+        jsonContent = JsonConvert.SerializeObject(body);
+        contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        //Act
+        returns = await client.PutAsync(serv,contentString);
+
+        //Assert
+        Assert.False(returns.IsSuccessStatusCode);
     }
 
 }
