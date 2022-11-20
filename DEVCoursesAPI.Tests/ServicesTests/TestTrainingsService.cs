4399d84545cb5535f79a3d3bbe19f8acb5126e76
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-20 11:30:16 -0300

    feat: add tests for GetUsersRegisteredInTraining and GetReports methods

--- DEVCoursesAPI.Tests/ServicesTests/TestTrainingsService.cs
+++ DEVCoursesAPI.Tests/ServicesTests/TestTrainingsService.cs
@@ -1,4 +1,5 @@
-﻿using DEVCoursesAPI.Data.DTOs.ModuleDTO;
+﻿using DEVCoursesAPI.Data.DTOs;
+using DEVCoursesAPI.Data.DTOs.ModuleDTO;
 using DEVCoursesAPI.Data.DTOs.TrainingDTO;
 using DEVCoursesAPI.Data.Models;
 using DEVCoursesAPI.Repositories;
@@ -124,6 +125,46 @@ namespace DEVCoursesAPI.Tests.ServicesTests
 
             // Assert
             Assert.Equal(noActiveStudent, result);
+        }
+
+        [Fact]
+        public async void GetUsersRegisteredInTraining_ShouldReturnRegisteredUsers()
+        {
+            // Arrange
+            RegisteredUsers registeredUsers = new ();
+
+            _trainingsRepository.Setup(repo => repo.GetUsersRegisteredInTraining(It.IsAny<Guid>()))
+                .ReturnsAsync(registeredUsers);                                
+
+            TrainingService service = new(_trainingsRepository.Object, _modulesService.Object);
+
+            // Act
+            RegisteredUsers result = await service.GetUsersRegisteredInTraining(Guid.NewGuid());
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async void GetReports_ShouldReturnListOfReportsOrderedByTotalFinishedStudents()
+        {
+            // Arrange
+            List<TrainingReport> reports = new()
+            {
+                new TrainingReport() { TotalFinishedStudents = 1 },
+                new TrainingReport() { TotalFinishedStudents = 10 },
+            };
+
+            _trainingsRepository.Setup(repo => repo.GetReports())
+                .ReturnsAsync(reports);
+
+            TrainingService service = new(_trainingsRepository.Object, _modulesService.Object);
+
+            // Act
+            List<TrainingReport> result = await service.GetReports();
+
+            // Assert
+            Assert.Equal(10, result.First().TotalFinishedStudents);
         }
     }
 }
