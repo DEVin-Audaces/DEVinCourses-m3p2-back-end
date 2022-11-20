6a925ad47993c1ef4a2eddbfb4a01e0bd2a74e98
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-19 21:33:13 -0300

    fix: remove wrong using

--- DEVCoursesAPI/Data/Models/Training.cs
+++ DEVCoursesAPI/Data/Models/Training.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.DataAnnotations;
 using DEVCoursesAPI.Data.DTOs.TrainingDTO;
 using DEVCoursesAPI.Data.DTOs.ModuleDTO;
-using System.Text.Json.Serialization;
 
 namespace DEVCoursesAPI.Data.Models
 {
